988ddd1008af51794fef31f06b03f98749425f8a
Chris Dickson <70047029+DicksonChris@users.noreply.github.com>
2022-05-29 21:37:02 -0500

    Changes search card styling

--- src/components/search/SearchCard.jsx
+++ src/components/search/SearchCard.jsx
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom'
 const SearchCard = ({ avatar, username, admin }) => {
   return (
     <Link to={`/user/${username}`}>
-      <div className='card shadow-xl bg-base-content text-primary-content flex flex-row md:flex-col m-0 p-0'>
+
+      <div className='card border-2 border-slate-800 shadow-xl bg-base-300 text-primary-content flex flex-row md:flex-col m-0 p-0'>
         <figure>
           <img
             src={avatar}
             alt={`${username}'s avatar`}
-            className='w-full min-w-24 h-24 md:h-max m-2 rounded-box bg-white  shadow-lg'
+            className='w-full min-w-24 h-24 md:h-max m-2 rounded-box bg-white shadow-lg'
           />
         </figure>
 
-        <div className='card-body md:h-24 md:pt-4'>
+        <div className='card-body text-base-content md:h-24 md:pt-4'>
           <h2 className='card-title'>{username}</h2>
           {admin && <div className='badge badge-secondary'>Admin</div>}
         </div>
